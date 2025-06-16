261d5402fc47beb151bc7afa42a146e6ce9a4cd8
Rakesh K H <Rakeshkh@outlook.in>
2025-06-16 22:56:33 +0530

    kube.sh

--- kube.sh
+++ kube.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 #docker 
-sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo systemctl enable --now docker
-sudo chmod +x kube.sh
-sudo usermod -aG docker ec2-user
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl enable --now docker
+chmod +x kube.sh
+usermod -aG docker ec2-user
 
 #kubectl
-
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-sudo mv kubectl /usr/local/bin/kubectl
+mv kubectl /usr/local/bin/kubectl
 kubectl version
 
 #eksctl
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+mv /tmp/eksctl /usr/local/bin
