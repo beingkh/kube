00f880dd3875f4d05cc3df8aa0f45da1af0f154c
Rakesh K H <Rakeshkh@outlook.in>
2025-06-16 20:31:07 +0530

    docker.sh

---
+++ ec2.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "kube"{
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.kubesg.id]
+}
+resource "aws_security_group" "kubesg" {
+  name        = "kubesg"
+  description = "Allowing all traffic"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
