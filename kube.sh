cf625b1a0c968375f78ae8568835443c51ed05a3
Rakesh K H <Rakeshkh@outlook.in>
2025-06-16 22:19:49 +0530

    kube.sh

--- kube.sh
+++ kube.sh
@@ -8,4 +8,11 @@ sudo systemctl enable --now docker
 sudo chmod +x kube.sh
 sudo usermod -aG docker ec2-user
 
-#kubernetes
+#kubectl
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+kubectl version
+
+#eksctl
